d834f1590428c7a32f17f138bbac21edec4d0611
Sam Gabbard <sam@ipinwheel.com>
2023-02-12 09:43:22 -0800

    updates to catalog slice

--- reactclient/src/features/catalog/Catalog.tsx
+++ reactclient/src/features/catalog/Catalog.tsx
@@ -1,21 +1,19 @@
-import { useState, useEffect } from "react";
-import agent from "../../app/api/agenct";
+import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Product } from "../../app/models/products";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
 
 function Catalog() {
-    const [products, setProducts] = useState<Product[]>([]);
-
-    const [loading, setLoading] = useState(true);
+    const products = useAppSelector(productSelectors.selectAll);
+    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
-        agent.Catalog.list().then(products => setProducts(products))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-    }, [])
+        if (!productsLoaded) dispatch(fetchProductsAsync());
+    }, [productsLoaded, dispatch, products])
 
-    if (loading) return <LoadingComponent message="Loading products..."/>
+    if (status.includes('pending')) return <LoadingComponent message="Loading products..."/>
 
     return (
         <>
